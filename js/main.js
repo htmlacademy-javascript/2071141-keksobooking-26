165400ac23cf0f540d035126d765cee33a2fa62f
Петр <kravchenko.pb.13@gmail.com>
2022-08-13 15:34:24 +0300

    Завершил 5 модуль

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import './utils.js';
+import './mocks.js';
+import { getMultipleBookings } from './mocks.js';
+
+console.log(getMultipleBookings());
---
+++ js/utils.js
@@ -0,0 +1,17 @@
+export const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+export const getRandomPositiveFloat = (a, b, digits = 1) => {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+};
+
+export const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
